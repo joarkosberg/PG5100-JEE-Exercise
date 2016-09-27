ae8d146928d5abfed96d0618521ff05f541fcbda
Joar <joar_kosberg@hotmail.com>
2016-09-27 15:18:11 +0200

    Cleaned upp pom file and setup project for deployment

--- src/main/java/data/Statistics.java
+++ src/main/java/data/Statistics.java
@@ -1,9 +1,29 @@
 package data;
 
+import javax.ejb.EJB;
+import javax.ejb.Schedule;
 import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Singleton
+@Startup
 public class Statistics {
+    private final AtomicInteger counter = new AtomicInteger(0);
 
+    @EJB
+    private UserBean userBean;
 
+    @Schedule(second = "10", minute="*", hour="*", persistent=false)
+    public void doSomeComputation(){
+        //TODO computation here
+
+        /*
+            Create a @Singleton with a time service that does query the database (eg every 10 seconds)
+            for overall statistics, and save them internally in the bean. (This is done to avoid having
+            100k users doing such expensive operation at the same time, ie we can consider this
+            singleton as a type of cache.)
+         */
+
+    }
 }
