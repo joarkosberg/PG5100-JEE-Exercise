6b54ccaf17ee9799c2fdc7427f68963f7a220554
Joar <joar_kosberg@hotmail.com>
2016-10-09 15:40:45 +0200

    Can now register User and show own data on home page for users.

--- ExamExample/frontend/src/main/java/controller/UserController.java
+++ ExamExample/frontend/src/main/java/controller/UserController.java
@@ -2,11 +2,13 @@ package controller;
 
 import ejb.UserEJB;
 import entity.User;
+import enums.CountryName;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.Arrays;
 
 @Named
 @SessionScoped
@@ -17,10 +19,19 @@ public class UserController implements Serializable {
 
     private String formUsername;
     private String formPassword;
+    private String formConfirmPassword;
+    private String formFirstName;
+    private String formMiddleName;
+    private String formLastName;
+    private String formCountryName;
 
     private User activeUser;
 
     public UserController(){
+    }
+
+    public String[] getCountryNames() {
+        return Arrays.stream(CountryName.values()).map(Enum::name).toArray(String[]::new);
     }
 
     public String login(){
@@ -33,7 +44,28 @@ public class UserController implements Serializable {
         }
     }
 
-    public String logOut(){
+    public String registerNew(){
+        if(! formPassword.equals(formConfirmPassword))
+            return "newUser.jsf";
+
+        CountryName countryName;
+        try {
+            countryName = CountryName.valueOf(formCountryName);
+        } catch (IllegalArgumentException ex){ //Not valid country
+            return "newUser.jsf";
+        }
+        User user = userEJB.createNewUser(formUsername, formPassword, formFirstName, formMiddleName,
+                formLastName, countryName);
+
+        if(user != null){
+            activeUser = user;
+            return "home.jsf";
+        } else {
+            return "newUser.jsf";
+        }
+    }
+
+    public String logout(){
         activeUser = null;
         return "home.jsf";
     }
@@ -64,5 +96,46 @@ public class UserController implements Serializable {
 
     public void setActiveUser(User activeUser) {
         this.activeUser = activeUser;
+    }
+
+
+    public String getFormConfirmPassword() {
+        return formConfirmPassword;
+    }
+
+    public void setFormConfirmPassword(String formConfirmPassword) {
+        this.formConfirmPassword = formConfirmPassword;
+    }
+
+    public String getFormFirstName() {
+        return formFirstName;
+    }
+
+    public void setFormFirstName(String formFirstName) {
+        this.formFirstName = formFirstName;
+    }
+
+    public String getFormMiddleName() {
+        return formMiddleName;
+    }
+
+    public void setFormMiddleName(String formMiddleName) {
+        this.formMiddleName = formMiddleName;
+    }
+
+    public String getFormLastName() {
+        return formLastName;
+    }
+
+    public void setFormLastName(String formLastName) {
+        this.formLastName = formLastName;
+    }
+
+    public String getFormCountryName() {
+        return formCountryName;
+    }
+
+    public void setFormCountryName(String formCountryName) {
+        this.formCountryName = formCountryName;
     }
 }
